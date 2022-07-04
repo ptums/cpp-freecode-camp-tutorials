bdd92cf60abc97aa52b556b769aafbd9deadd388
ptums <ptumulty923@gmail.com>
2022-07-04 09:15:37 -0400

    aded multi dimensional array lessons

--- pointers/multi-dimensional-arrays.cpp
+++ pointers/multi-dimensional-arrays.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 int main () 
 {
+
+  // multi-dimensional array
+  int row, col;
+  cout << "rows, cols";
+  cint >> rows >> cols;
+  
+  int **table = new int*[rows];
+
+  for(int i = 0; i < rows; i++)
+  {
+    table[i] = new int [cols];
+  }
+
+  // will store 88 in the row 1 and column 2
+  table[1][2] = 88;
+
+  // now we need to deallocate the memory
+  // since we aren't using it anymore
+  for(int i = 0; i < rows; i++)
+  {
+    delete[] table[i];
+  }
+
+  delete[] table;
+  table = NULL;
+
+
+  // once you allocate memory using pointers
+  // you need to delete that memory too. DONT FORGET
   // https://youtu.be/kiUGf_Z08RQ?t=4257
   return 0;
 }
