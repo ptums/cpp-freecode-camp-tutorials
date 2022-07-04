ef68112c63899a13dbdaab8046b8f1d03f4185d4
Peter Tumulty <ptumulty923@gmail.com>
2022-07-04 00:28:04 -0400

    left off on dynamic arrays

---
+++ pointers/dynamic-arrays-pointers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+
+  return 0;
+  // https://www.youtube.com/watch?v=kiUGf_Z08RQ&ab_channel=CodeBeauty
+}
--- pointers/multiple-values-functions-with-pointers.cpp
+++ pointers/multiple-values-functions-with-pointers.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 using namespace std;
 
+int getMin(int nums[], int size) {
+  int min =  nums[0];
+
+  for(int i = 1; i <  size; i++) {
+    if(nums[i] < min) {
+      min = nums[i];
+    }
+  }
+  return min;
+}
+
+
+
+int getMax(int nums[], int size) {
+  int max =  nums[0];
+
+  for(int i = 1; i <  size; i++) {
+    if(nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  return max;
+}
+
+
+// now we do this using pointers and referencing
+// this is pretty wild!
+void getMinAndMax(int numbers[], int size, int* max, int* min) 
+{
+  for(int i = 1; i < size; i++) {
+    if(numbers[i] > *max) {
+      *max = numbers[i];
+    }
+
+    if(numbers[i] < *min) {
+      *min = numbers[i];
+    }
+  }
+
+}
 int main () 
 {
+  int nums[5] = { 5, 4, -2, 29, 6 };
+  
+  cout << "Min is " << getMin(nums, 5) << endl;
+  cout << "Max is " << getMax(nums, 5) << endl;
+
+  cout << endl;
+
+  int min = nums[0];
+  int max = nums[0];
+
+  getMinAndMax(nums, 5, &max, &min);
+
+  cout << "min: " << min << endl;
+  cout << "max: " << max << endl;
+   
+
   return 0;
 }
