c37dc176a2828f1c8e487bcbb8be5e1938a0a19f
ptums <ptumulty923@gmail.com>
2022-07-04 07:56:00 -0400

    finished new and delete arrays

--- pointers/dynamic-arrays-pointers.cpp
+++ pointers/dynamic-arrays-pointers.cpp
@@ -4,6 +4,37 @@ using namespace std;
 int main () 
 {
 
-  return 0;
-  // https://www.youtube.com/watch?v=kiUGf_Z08RQ&ab_channel=CodeBeauty
+  // an array is a type of collection which stores elements in continuous memory
+  // it stores elements one after another
+
+  // we are setting 5 containers with the data type of int
+  // in memory
+  int myArr[5];
+
+  // dynamic size arrays
+  // There is a way to allocate and deallocate dynamic amount
+  // of memory
+  int size;
+  cout << "Size: ";
+  cin >> size;
+  int* myDynArray = new int[size];
+
+  for(int i = 0; i < size; i++){
+    cout << "myDynArray[" << i << "]";
+    cin >> myDynArray[i];
+  }
+
+  for(int i = 0; i < size; i++){
+   // cout << myDynArray[i] << endl;
+   // this is the derefencing pointer approach
+   cout << *(myDynArray +i) << endl;
+  }
+
+
+  delete[]myDynArray;
+  myDynArray = NULL;
+
+
+
+ return 0;
 }
---
+++ pointers/multi-dimensional-arrays.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+  // https://youtu.be/kiUGf_Z08RQ?t=4257
+  return 0;
+}
